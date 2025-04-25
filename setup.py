f5410e40dbb9c545e73063ec6062c710b0436447
codewithzaqar <zaqarstandoff@gmail.com>
2025-04-25 21:57:25 +0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="consh",
-    version="0.03",
+    version="0.04",
     packages=find_packages(),
     install_requires=["prompt_toolkit>=3.0.0"],
     entry_points={
